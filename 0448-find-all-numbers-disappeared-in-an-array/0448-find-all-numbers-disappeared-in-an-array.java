76c467374cf91fe29b4495e62bf6547217833771
Satyam Jat <satyam.2025ca086@mnnit.ac.in>
2026-03-15 11:02:20 +0530

    Time: 4 ms (96.33%), Space: 70.7 MB (36.24%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-      Set<Integer>al=new TreeSet<>();
-      ArrayList<Integer>am=new ArrayList<>();
-        int n=nums.length;
-        for(int i=0;i<n;i++)
-        {
-         al.add(nums[i]);
-        }
-        for(int i=1;i<=n;i++)
-        {
-        if(!al.contains(i))
-         am.add(i);
-        }
-        return am;
+      int n=nums.length;
+      ArrayList<Integer> al=new ArrayList<>();
+      int freq[]=new int[n+1];
+      for(int i=0;i<n;i++)
+      {
+        freq[nums[i]]++;
+      }
+      for(int i=1;i<=n;i++)
+      {
+        if(freq[i]==0) al.add(i);
+      }
+      return al;
     }
 }

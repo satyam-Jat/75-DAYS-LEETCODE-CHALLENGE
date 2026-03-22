99410aa9b2c624c50faa28a2ef1e1a2a5fa31ab9
Satyam Jat <satyam.2025ca086@mnnit.ac.in>
2026-03-22 12:26:24 +0530

    Time: 2 ms (96.33%), Space: 48.2 MB (87.7%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,14 +4,18 @@ class Solution {
         int arr[]=new int[2];
         int low=0;
         int high=n-1;
+       
         while(low<high)
         {
-            if(numbers[low]+numbers[high]==target) return new int[]{low+1,high+1};
+            if(numbers[low]+numbers[high]==target) 
+            {arr[0]=low+1;
+            arr[1]=high+1;
+            return arr;} 
             else if(numbers[low]+numbers[high]>target) high--;
             else{
                 low++;
             }
         }
-        return new int[]{};
+        return arr;
     }
 }

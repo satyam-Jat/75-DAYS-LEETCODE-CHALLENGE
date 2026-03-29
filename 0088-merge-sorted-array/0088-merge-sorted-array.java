869a0fdb025ff41df2ac504d58323fdcee772f22
Satyam Jat <satyam.2025ca086@mnnit.ac.in>
2026-03-29 22:28:10 +0530

    Time: 0 ms (100%), Space: 43.8 MB (70.06%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,25 +1,15 @@
-class Solution{
-public void merge(int[] arr, int m, int[] brr, int n) {
-    int[] temp = new int[m];
-    for (int i = 0; i < m; i++) {
-        temp[i] = arr[i]; //temp me arr dal rhe
-    }
-    int i = 0; 
-    int k = 0;
-    int j = 0; 
-    while (k < m && j < n) {
-        if (temp[k] <= brr[j]) {  //jo chota hai usko arr me dalte jao
-            arr[i++] = temp[k++];
-        } else {
-            arr[i++] = brr[j++];
+class Solution {
+    public void merge(int[] arr1, int m, int[] arr2, int n) {
+        int r1 = m - 1;        // last element in arr1
+        int r2 = n - 1;        // last element in arr2
+        int w  = m + n - 1;    // write pointer (end of arr1)
+
+        while (r2 >= 0) {   // only arr2 check is enough
+            if (r1 >= 0 && arr1[r1] > arr2[r2]) {
+                arr1[w--] = arr1[r1--];
+            } else {
+                arr1[w--] = arr2[r2--];
+            }
         }
     }
-    while (k < m) {
-        arr[i++] = temp[k++]; // bche hue direct uthakr daldo,arr me kuch bhi ho sb override ho rha hai
-    }
-
-    while (j < n) {
-        arr[i++] = brr[j++]; //same
-    }
-}
 }

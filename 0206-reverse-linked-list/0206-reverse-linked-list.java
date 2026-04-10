469ac02b136a30b72506a4cd7b1f9cf2f96493ba
Satyam Jat <satyam.2025ca086@mnnit.ac.in>
2026-04-10 19:58:04 +0530

    Time: 0 ms (100%), Space: 44.4 MB (37.44%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode p=null; //prevoius pointer point to null(before headC)
+        ListNode c=head; //current me head
+        ListNode n=null; //head/null  //next
+        while(c!=null){
+            n=c.next; //these 4 lines are technique of pointers , c ka address le lia taki future me vo connection tode to bhi connect hojae
+            c.next=p; //ye pehle chla dia to connection tut jaega c ka ,kyuki ek time pe ek ko hi point krskta to null ko krdia
+            p=c;//current age bdhega to prevous se connection tut jaefa islie usme c ko la rhe
+            c=n; //pointer age bdate ja rhe current ka
+        }
+        return p;
+    }
+}

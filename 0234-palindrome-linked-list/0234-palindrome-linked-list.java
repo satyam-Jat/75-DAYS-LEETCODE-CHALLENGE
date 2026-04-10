00672e91471bd64f293131ea880bab0bdf20bf34
Satyam Jat <satyam.2025ca086@mnnit.ac.in>
2026-04-10 23:34:38 +0530

    Time: 7 ms (31.58%), Space: 96.5 MB (33.12%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,4 +1,4 @@
-        class Solution {
+       /* class Solution {
     public boolean isPalindrome(ListNode head) {
 
         ListNode temp = head;
@@ -22,4 +22,34 @@
 
         return true;
     }
-}
+} */
+//2nd method - nyi linked list(deep copy) bnao jisme node to nyi hogi bs value purane wali hogi , usko reverse function me dalkr then check dono same hai ya nahi.
+//3rd method
+ class Solution {
+ public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head; //base case
+        ListNode a=head.next; // a ka pointer baha rkh dia jaha tk recursion le ayega reverse krke
+        ListNode newHead=reverseList(a); //last m hoga bahi nya head recursive function m.
+        a.next=head; //changing pointing nodes
+        head.next=null;
+        return newHead;
+    }
+    public boolean isPalindrome(ListNode head) {
+       ListNode slow=head;
+       ListNode fast=head;
+       while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+       }
+       
+        ListNode i=head;
+        ListNode j=reverseList(slow);
+        while(j!=null)
+        {
+            if(i.val!=j.val) return false;
+            i=i.next;
+            j=j.next;
+        }
+        return true;
+    }
+ }

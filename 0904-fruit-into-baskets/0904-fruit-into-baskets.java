50cebc722805b304b0136bd0d267e57fe4e826ba
Satyam Jat <satyam.2025ca086@mnnit.ac.in>
2026-08-21 19:52:54 +0530

    Time: 54 ms (66.75%), Space: 68.9 MB (83.09%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,16 +1,62 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+        int n=fruits.length;
+        Map<Integer,Integer>map=new HashMap<>();
+        int i=0,j=0; int maxlen=Integer.MIN_VALUE; int length=0;
+        while(j<n){
+             map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+            if(map.size()>2){ //jb 2 se bdha hoga to hi to apn ko condition lgani hai . brna add krte jao jb tk apni condition break ni hori.
+                map.put(fruits[i],map.get(fruits[i])-1);
+                if(map.get(fruits[i])==0){
+                    map.remove(fruits[i]);
+                }
+                i++;
+            }
+            length=j-i+1;
+            j++;
+            if(maxlen<length){
+                maxlen=length;
+            }
+    }
+ return maxlen;   
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
+    public int totalFruit(int[] fruits) {
         int n=fruits.length; int maxlen=0;
         int i=0; int j=0; 
         HashMap<Integer,Integer>map=new HashMap();
-        while(j<n){
-            if(map.containsKey(fruits[j])){
+        while(j<n){ //sliding window
+            if(map.containsKey(fruits[j])){ //arr[j] freq add krne ka kaam krra
                 map.put(fruits[j],map.get(fruits[j])+1);
             }
             else{
-                map.put(fruits[j],1);
+                map.put(fruits[j],1); 
             }
-           while(map.size()>2){
+           while(map.size()>2){ //arr[i] aage se elemts htane ka kaam krra
             int freq=map.get(fruits[i]);
             if(freq==1){
                 map.remove(fruits[i]);
@@ -27,3 +73,4 @@ class Solution {
         return maxlen;
     }
 }
+*/
